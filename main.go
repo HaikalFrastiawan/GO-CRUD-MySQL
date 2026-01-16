3c4b207f0e0bb4c206a1ca239de35e62a8148160
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-01-17 06:19:00 +0700

    index product

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"go-web-native/config"
 	"go-web-native/controller/categorycontroller"
 	"go-web-native/controller/homecontroller"
-	"log"
-"net/http"
+	"go-web-native/controller/productcontroller"
 )
 
 func main() {
@@ -20,6 +21,13 @@ func main() {
 	http.HandleFunc("/categories/add", categorycontroller.Add)
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	//product
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
+
 
 	log.Println("Server Running on port 8080")
 	http.ListenAndServe(":8080", nil)
